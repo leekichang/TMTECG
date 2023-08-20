48d941d43c83a15765607ef7fcc3bae029791ad6
leekichang <kichang.lee@yonsei.ac.kr>
2023-08-20 19:06:24 +0900

    code clean up

--- BYOL.py
+++ BYOL.py
@@ -123,41 +123,12 @@ class BYOL:
     @torch.no_grad()
     def test(self):
         return
-        self.model.eval()
-        preds, targets, losses = [], [], []
-        for X, Y in self.test_loader:
-            X, Y = X.to(self.device), Y.to(self.device)
-            pred = self.model(X)
-            loss = self.criterion(pred, Y)
-            
-            preds.append(pred.cpu().numpy())
-            targets.append(Y.cpu().numpy())
-            losses.append(loss.item())
-            
-        preds = np.concatenate(preds)
-        targets = np.concatenate(targets)
-        
-        acc    = utils.calculate_topk_accuracy(torch.from_numpy(preds), torch.from_numpy(targets), k=1)
-        recall = recall_score(y_true=targets, y_pred=np.argmax(preds, axis=-1))
-        f1     = f1_score(y_true=targets, y_pred=np.argmax(preds, axis=-1))
-        spec   = utils.specificity_score(y_true=targets, y_pred=np.argmax(preds, axis=-1))
-
-        
-        self.test_loss.append(np.mean(losses))
-        self.accs.append(acc)
-        self.recalls.append(recall)
-        self.f1s.append(f1)
-        self.specs.append(spec)
-        self.TB_WRITER.add_scalar(f'Test Loss', np.mean(losses), self.epoch+1)
-        self.TB_WRITER.add_scalar(f'Test Accuracy', acc, self.epoch+1)
     
     def save_model(self):
         torch.save(self.online_model.state_dict(), f'{self.save_path}/{self.epoch+1}.pth')
 
     def print_train_info(self):
         print(f'({self.epoch+1:03}/{self.epochs}) Train Loss:{self.train_loss[self.epoch]:>6.4f}', flush=True)
-        # print(f'({self.epoch+1:03}/{self.epochs}) Train Loss:{self.train_loss[self.epoch]:>6.4f} Test Loss:{self.test_loss[self.epoch]:>6.4f} Test Accuracy:{self.accs[self.epoch]:>5.2f}%', flush=True)
-        # print(f'({self.epoch+1:03}/{self.epochs}) recall:{self.recalls[self.epoch]:>6.2f} f1:{self.f1s[self.epoch]:>6.4f} specification:{self.specs[self.epoch]:>5.2f}%', flush=True)
 
 if __name__ == '__main__':
     from tqdm import tqdm
